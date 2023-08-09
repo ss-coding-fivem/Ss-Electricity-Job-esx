e68029058acbd62d403f243bdb7569d48d99091f
SS CODING <126167764+ss-coding-fivem@users.noreply.github.com>
2023-08-10 01:08:54 +0530

    Update __resource.lua

--- __resource.lua
+++ __resource.lua
@@ -1,6 +1,6 @@
-author 'Roderic'
-description 'Electricist Job'
-author 'Roderic#0614'
+author 'Ss CODING'
+description 'SKY Electricist Job'
+author 'AKJK'
 resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
 
 client_script {
